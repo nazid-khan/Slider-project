d4bf8b523d0e0b95b52be620c507d8225f48b6a1
nazid <nazidk773@gmail.com>
2024-09-11 12:29:27 +0530

    slide functionality

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 const slides = document.querySelectorAll(".slide");
 const prevBtn = document.querySelector(".prevBtn");
 const nextBtn = document.querySelector(".nextBtn");
+
+slides.forEach(function(slide,index){
+    slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+
+nextBtn.addEventListener("click",function(){
+    counter++;
+    corousel();
+});
+
+prevBtn.addEventListener("click",function(){
+    counter--;
+    corousel();
+});
+
+function corousel(){
+    // working with slide
+    if(counter === slides.length){
+        counter = 0;
+    }
+    if(counter < 0){
+        counter = slides.length -1;
+    }
+
+    slides.forEach(function(slide){
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+}
